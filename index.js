57d91277f563479fa46f4b74cbdc893755ee9b3b
Adam <adamwatters11@gmail.com>
2014-11-03 17:00:07 -0500

    lots of changes, trying to get heroku to work

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + "/public"));
 
 var urlKeys = [];
 
-// app.get("/longURL", function(request, response) {
-// 	var longURL = request.query.link;
-// 	var key = Math.floor(1000 * Math.random());
-// 	urlKeys[key] = {longURL: longURL, visits: 0};
+app.get("/longURL", function(request, response) {
+	var longURL = request.query.link;
+	var key = Math.floor(1000 * Math.random());
+	urlKeys[key] = {longURL: longURL, visits: 0};
 
-// 	var shortURL = "localhost:8080/r?k=" + key;
-// 	response.render("shortURL.ejs", {shortURL: shortURL});
-// });
+	var shortURL = "localhost:8080/r?k=" + key;
+	response.render("shortURL.ejs", {shortURL: shortURL});
+});
 
 app.get("/r", function(request, response) {
 	var key = request.query.k;
