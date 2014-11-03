550fafe0b4a0bab099a67cae932495fb25cf388f
Adam <adamwatters11@gmail.com>
2014-11-03 16:45:30 -0500

    llaksdjfa

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express')
+var express = require("express");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+var urlKeys = [];
+
+// app.get("/longURL", function(request, response) {
+// 	var longURL = request.query.link;
+// 	var key = Math.floor(1000 * Math.random());
+// 	urlKeys[key] = {longURL: longURL, visits: 0};
+
+// 	var shortURL = "localhost:8080/r?k=" + key;
+// 	response.render("shortURL.ejs", {shortURL: shortURL});
+// });
+
+app.get("/r", function(request, response) {
+	var key = request.query.k;
+	response.redirect(urlKeys[key].longURL);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
