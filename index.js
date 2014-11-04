ded017e306cca212de2c4f14d4765a1092421982
Adam <adamwatters11@gmail.com>
2014-11-03 21:53:01 -0500

    data to redis database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var app = express();
 
+if (process.env.REDISTOGO_URL) {
+    // deployed
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	//split username and password
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+	//development
+    var redis = require("redis").createClient();
+}
 
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
@@ -8,13 +18,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
-var urlKeys = [];
-
 app.get("/longURL", function(request, response) {
 	var longURL = request.query.link;
-	var key = Math.floor(1000 * Math.random());
-	urlKeys[key] = {longURL: longURL, visits: 0};
-
+	var key = Math.random().toString(36).slice(13);
+	redis.set(key, longURL);
 	var domain = process.env.DOMAIN;
 	var shortURL = domain + "/r?k=" + key;
 	response.render("response.ejs", {shortURL: shortURL});
@@ -22,7 +29,13 @@ app.get("/longURL", function(request, response) {
 
 app.get("/r", function(request, response) {
 	var key = request.query.k;
-	response.redirect(urlKeys[key].longURL);
+	redis.get(key, function(err, data){
+		if(err){
+			console.log(err);
+		} else {
+			response.redirect(data);
+		}
+	});
 });
 
 app.listen(app.get('port'), function() {
